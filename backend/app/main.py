7316dc330a1ef0519072b977f21d5aef3c3d4f8e
dogtype <jonas.hofmann1@tu-darmstadt.de>
2026-05-24 22:09:33 +0200

    authentication test

--- backend/app/main.py
+++ backend/app/main.py
@@ -468,7 +468,11 @@ def delete_furniture(item_id: int, _=Depends(verify_token)):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://dogtype.github.io",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
